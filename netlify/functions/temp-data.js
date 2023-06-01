173e5cb945b4a98f11235768f785c303bc4438e1
jcipriano <>
2023-06-01 13:11:26 -0700

    Changes dir to os.tmpdir

--- netlify/functions/temp-data.js
+++ netlify/functions/temp-data.js
@@ -1,27 +1,32 @@
 const fs = require('fs');
+const os = require('os');
 
 let updatedCount;
+const tmpDir = os.tmpdir() + '/tmp';
+const fileDataPath = tmpDir + '/counter.txt';
 
 function attemptTmpWriting(){
   try {
 
-    const exists = fs.existsSync('/tmp');
-    const fileExists = fs.existsSync('/tmp/counter.txt');
+    const exists = fs.existsSync(tmpDir);
+    const fileExists = fs.existsSync(fileDataPath);
 
     if(!exists){
-      fs.mkdirSync('/tmp', { recursive: true });
+      console.log('directory does not exist, making it')
+      fs.mkdirSync(tmpDir, { recursive: true });
     }
 
     let count = 0;
     if (fileExists) {
-      const countTxt = fs.readFileSync('/tmp/counter.txt', 'utf-8');
+      console.log('file exists')
+      const countTxt = fs.readFileSync(fileDataPath, 'utf-8');
       if (countTxt !== undefined) {
         count = parseInt(countTxt, 10);
       }
     }
 
     updatedCount = ++count;
-    fs.writeFileSync('/tmp/counter.txt', (updatedCount).toString());
+    fs.writeFileSync(fileDataPath, (updatedCount).toString(), { encoding: 'utf8' });
 
   } catch (e) {
     console.error(e);
@@ -38,6 +43,6 @@ exports.handler = async function (event, context) {
     headers: {
       'Access-Control-Allow-Origin': '*'
     },
-    body: JSON.stringify({updatedCount}, null, 2)
+    body: JSON.stringify({updatedCount, tmpDir}, null, 2)
   };
 }
